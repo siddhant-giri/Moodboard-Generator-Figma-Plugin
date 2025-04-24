77bc6c00a51dda60dc580081e99155fe76cb354c
Siddhant Giri <39070056+siddhant-giri@users.noreply.github.com>
2025-04-25 02:14:04 +0530

    Update config.ts

--- src/plugin/config.ts
+++ src/plugin/config.ts
@@ -1,10 +1,1 @@
-export const config = {
-  PEXELS_API_KEY: 'kDTCToC4gwx1QxZVSRkkSXTi39Z9HNHqEhTzc3erG8PBYlOaiUFmdsml', // Get from https://www.pexels.com/api/
-  GOOGLE_FONTS_API_KEY: 'AIzaSyD9l61hR0JNZ8-a3naIzhqKpQWfsmHjr_o' // Optional: for font suggestions
-};
 
-export const GOOGLE_CONFIG = {
-  API_KEY: 'AIzaSyBhTNFbYwO44BIVp4mSA33YQABO_Sdzj-c',
-  SEARCH_ENGINE_ID: '628107ebb9bc34c09',
-  API_URL: 'https://www.googleapis.com/customsearch/v1'
-}; 
